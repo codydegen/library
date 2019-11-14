facdcd60ca40359871bcdfe260e8d03e5a23a93d
codydegen <codydegen@gmail.com>
2019-11-14 15:45:56 -0500

    Add new book functionality

--- myscripts.js
+++ myscripts.js
@@ -12,7 +12,7 @@ return this.title+" by "+this.author+", "+this.pages+", "+(this.read?" read":" h
 
 };
 
-function addBookToLibrary() {
+function addBookToLibraryManual() {
 let title = prompt("what is the title?");
 let read = prompt("what is the read?");
 let pages = prompt("what is the pages?");
@@ -22,11 +22,17 @@ myLibrary.push(book);
 render(myLibrary.length - 1);
 };
 
-addBookToLibrary();
+function addBookToLibrary(title, author, pages, read) {
+  let book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+  render(myLibrary.length - 1);
+  };
+
+// addBookToLibrary();
 let book = new Book("bob", "grrm", "23", "true");
 myLibrary.push(book);
 
-render(1);
+render(0);
 function render(book) {
   const mainContainer = document.querySelector("#mainContainer");
   console.log(myLibrary[book].info());
@@ -43,7 +49,7 @@ function render(book) {
   for(let i = 0;i<propertyList.length;i++) {
     let insideNode = document.createElement("li");
     insideNode.textContent = propertyList[i][0] + ": " + propertyList[i][1];
-    console.log(textNode)
+    // console.log(textNode)
     list.appendChild(insideNode);
   }
   // node.appendChild(list);
@@ -54,4 +60,27 @@ function render(book) {
     //   console.log(key, myLibrary[i][key]);
     // }
 
+};
+
+function formHandle() {
+  let x = document.getElementById("form1");
+  let content = "";
+  let label = "";
+  let i;
+  for (i=0; i<x.length; i++) {
+    content = x.elements[i].value;
+    console.log(content);
+    // text += x.elements[i].value + "<br>";
+  }
+  let title = x.elements[0].value;
+  let author = x.elements[1].value;
+  let pages = x.elements[2].value;
+  let read = x.elements[3].checked;
+  addBookToLibrary(title, author, pages, read);
+  showForm(false);
+  // console.log( text )
+};
+
+function showForm(visibility) {
+  document.getElementById("formContainer").style.visibility = visibility ? "visible" : "hidden";
 };
